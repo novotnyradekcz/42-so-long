79cabbd1e184491b8776db772180673ddd3afba8
Radek Novotny <rano@ciklum.com>
2023-11-02 09:12:26 +0100

    Started input checking

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 14:11:44 by rnovotny          #+#    #+#             */
-/*   Updated: 2023/10/18 14:12:12 by rnovotny         ###   ########.fr       */
+/*   Updated: 2023/11/01 17:00:00 by rnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 int	main(int argc, char **argv)
 {
+	int	fd;
+	
+	if (argc != 1)
+	{
+		write(2, "Error\n", 6);
+		write(2, "Please pass a valid .ber map as a parameter.", 44);
+	}
+	fd = open(argv[1], O_RDONLY);
 	return (0);
 }
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 14:10:09 by rnovotny          #+#    #+#             */
-/*   Updated: 2023/10/18 14:11:41 by rnovotny         ###   ########.fr       */
+/*   Updated: 2023/11/01 16:53:23 by rnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 
 int	main(int argc, char **argv);
 
